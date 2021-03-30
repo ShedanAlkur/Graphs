671cec06a7ff08e1dec2cd2f94b7c60fb40e54c2
Andrey Shedlbauer <shedlbauerand@gmail.com>
2021-03-30 14:43:37 +0300

    Добавление структуры NodeData, описывающей визуальную часть вершины графа

---
+++ GraphControl/NodeData.cs
@@ -0,0 +1,44 @@
+﻿using GraphLib;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace GraphControl
+{
+    public struct NodeData
+    {
+        public Ellipse Ellipse;
+        public Vector2 Position
+        {
+            get => Ellipse.Middle;
+            set { Ellipse.Middle = value; }        
+        }
+        
+        public void SetPosition(Vector2 value)
+        {
+            Ellipse.Middle = value;
+        }
+        public int Top { get => Ellipse.Top; }
+        public int Left { get => Ellipse.Left; }
+        public int Right { get => Ellipse.Right; }
+        public int Bottom { get => Ellipse.Bottom; }
+        public int Width { get => Ellipse.Width; }
+        public int Height { get => Ellipse.Height; }
+
+        public bool Collision(Vector2 point) =>
+            Ellipse.Contains(point);
+
+        internal void SetDiameter(int diameter)
+        {
+            Ellipse.SetSize(diameter);
+        }
+
+        public NodeData(Vector2 position, int diameter)
+        {
+            this.Ellipse = new Ellipse(0, 0, diameter, diameter);
+            this.Position = position;
+        }
+    }
+}
