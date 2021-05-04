d5855dd297e39a1275c650899f7e960969593604
Andrey Shedlbauer <shedlbauerand@gmail.com>
2021-05-04 06:25:57 +0300

    Правка классов для графического представления графа в GraphControl

--- GraphControl/EdgeData.cs
+++ GraphControl/EdgeData.cs
@@ -7,19 +7,90 @@ using System.Text;
 
 namespace GraphControl
 {
-    public struct EdgeData
+    public class EdgeData
     {
-        public int Width;
-        public Vector2 P1, P2;
+        private int width, nodeRadius;
 
+        /// <summary>
+        /// Координата начала ребра.
+        /// </summary>
+        public Vector2 P1;
+        /// <summary>
+        /// Координата конца ребра.
+        /// </summary>
+        public Vector2 P2;
+
+        /// <summary>
+        /// Возвращает или задает ширину ребра.
+        /// </summary>
+        public int Width
+        {
+            get { return width; }
+            set { width = value; }
+        }
+
+        /// <summary>
+        /// Инициализирует новый экземпляр класса.
+        /// </summary>
+        /// <param name="P1">Координата центра начальной вершины ребра.</param>
+        /// <param name="P2">Координата центра конечной вершины ребра.</param>
+        /// <param name="width">Ширина ребра.</param>
+        /// <param name="nodeRadius">Радиус вершин, которые связаны с ребром.</param>
+        public EdgeData(Vector2 P1, Vector2 P2, int width, int nodeRadius)
+        {
+            double angle = Math.Atan2(P1.Y - P2.Y, P1.X - P2.X);
+            Vector2 vectorWithinNode = Vector2.CreateByAngle(nodeRadius, angle); // Смещение на границу вершины.
+            Vector2 shift = new Vector2((int)(nodeRadius / 2 * Math.Sin(angle)), // Смещение от центральной линии вправо.
+                -(int)(nodeRadius / 2 * Math.Cos(angle)));
+            this.P1 = P1 - vectorWithinNode + shift;
+            this.P2 = P2 + vectorWithinNode + shift;
+            this.width = width;
+            this.nodeRadius = nodeRadius;
+        }
+
+        /// <summary>
+        /// Устанавливает новые координаты начала и конца ребра.
+        /// </summary>
+        /// <param name="P1">Координата начала ребра.</param>
+        /// <param name="P2">Координата конца ребра.</param>
+        public void SetPosition(Vector2 P1, Vector2 P2)
+        {
+            double angle = Math.Atan2(P1.Y - P2.Y, P1.X - P2.X);
+            Vector2 vectorWithinNode = Vector2.CreateByAngle(nodeRadius, angle);
+            Vector2 shift = new Vector2((int)(nodeRadius / 2 * Math.Sin(angle)),
+                -(int)(nodeRadius / 2 * Math.Cos(angle)));
+            this.P1 = P1 - vectorWithinNode + shift;
+            this.P2 = P2 + vectorWithinNode + shift;
+        }
+
+        /// <summary>
+        /// Определяет, лежит ли заданная точка на прямой
+        /// </summary>
+        /// <param name="pos"></param>
+        /// <returns>True - если заданная точка содержится на прямой, иначе False.</returns>
         public bool Collision(Vector2 pos)
         {
             if (pos.Y < Math.Min(P1.Y, P2.Y) || pos.Y > Math.Max(P1.Y, P2.Y))
                 return false;
-            float tg = (P2.Y - P1.Y) / (P2.X - P1.X);
-            float predictedY = P1.Y + tg * (pos.X - P1.X);
+            float ctg = ((float)P2.Y - P1.Y) / (P2.X - P1.X);
+            float predictedY = P1.Y + ctg * (pos.X - P1.X);
             bool located = Math.Abs(pos.Y - predictedY) <= Width;
+            //Console.WriteLine($"Ожидается {pos}, получено x = {pos.X} y = ({predictedY - Width};{predictedY + Width})");
+
             return located;
+        }
+
+        /// <summary>
+        /// Возвращает координату середины прямой.
+        /// </summary>
+        public Vector2 Middle
+        {
+            get { return P1 + (P2 - P1) / 2; }
+        }
+
+        public override string ToString()
+        {
+            return $"Edge: p1({P1}), p2({P2}), width = {Width}";
         }
     }
 }
--- GraphControl/NodeData.cs
+++ GraphControl/NodeData.cs
@@ -7,38 +7,90 @@ using System.Text;
 
 namespace GraphControl
 {
-    public struct NodeData
+    public class NodeData
     {
-        public Ellipse Ellipse;
-        public Vector2 Position
-        {
-            get => Ellipse.Middle;
-            set { Ellipse.Middle = value; }        
-        }
-        
-        public void SetPosition(Vector2 value)
-        {
-            Ellipse.Middle = value;
-        }
-        public int Top { get => Ellipse.Top; }
-        public int Left { get => Ellipse.Left; }
-        public int Right { get => Ellipse.Right; }
-        public int Bottom { get => Ellipse.Bottom; }
-        public int Width { get => Ellipse.Width; }
-        public int Height { get => Ellipse.Height; }
+        /// <summary>
+        /// Круг для заливки вершины.
+        /// </summary>
+        public Circle FillEllipse;
 
+        /// <summary>
+        /// Круг для обводки вершины;
+        /// </summary>
+        public Circle OutlineEllipse;
+
+        private int diameter;
+        private int outlineWidth;
+
+
+        //public int Top { get => FillEllipse.Top; }
+        //public int Left { get => FillEllipse.Left; }
+        //public int Right { get => FillEllipse.Right; }
+        //public int Bottom { get => FillEllipse.Bottom; }
+        //public int Diameter
+        //{ 
+        //    get => this.diameter;
+        //    set
+        //    {
+        //        FillEllipse.Diameter = diameter;
+        //        OutlineEllipse.Diameter = diameter - outlineWidth;
+        //    }
+        //}
+        //public int OutlineWidth
+        //{
+        //    get { return outlineWidth; }
+        //    set
+        //    {
+        //        outlineWidth = value;
+        //        OutlineEllipse.Diameter = diameter - outlineWidth;
+        //    }
+        //}
+
+        /// <summary>
+        /// Воозвращает или задает координату центра вершины.
+        /// </summary>
+        public Vector2 Middle 
+        { 
+            get => FillEllipse.Middle;
+            set
+            {
+                FillEllipse.Middle = value;
+                OutlineEllipse.Middle = value;
+            }
+        }
+
+        /// <summary>
+        /// Определяет,содержится ли заданная точка в вершине.
+        /// </summary>
+        /// <param name="point">Координата тестируемой точки</param>
+        /// <returns>True - если заданная точка содержится в вершине, иначе False.</returns>
         public bool Collision(Vector2 point) =>
-            Ellipse.Contains(point);
+            FillEllipse.Contains(point);
 
-        internal void SetDiameter(int diameter)
+        /// <summary>
+        /// Инициализирует новый экземпляр класса.
+        /// </summary>
+        /// <param name="middle">Воозвращает или задает координату центра вершины.</param>
+        /// <param name="diameter">Диаметр вершины.</param>
+        /// <param name="outlineWidth">Ширина обводки вершины.</param>
+        public NodeData(Vector2 middle, int diameter, int outlineWidth)
         {
-            Ellipse.SetSize(diameter);
+            this.outlineWidth = outlineWidth;
+            this.diameter = diameter;
+            int fillRadius = diameter / 2;
+            int outlineRadius = this.outlineWidth / 2;
+
+            this.FillEllipse =
+                new Circle(middle.X - fillRadius, middle.Y - fillRadius, diameter);
+            this.OutlineEllipse =
+                new Circle(middle.X + outlineRadius - fillRadius,
+                middle.Y + outlineRadius - fillRadius,
+                diameter - outlineRadius);
         }
 
-        public NodeData(Vector2 position, int diameter)
+        public override string ToString()
         {
-            this.Ellipse = new Ellipse(0, 0, diameter, diameter);
-            this.Position = position;
+            return $"FillEllipse: {FillEllipse}";
         }
     }
 }
